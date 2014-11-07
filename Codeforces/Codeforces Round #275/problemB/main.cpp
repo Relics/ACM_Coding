1dfb1915058ac646edbb8c256eb6b1e8d0dbfa45
Relics <lcpingping@gmail.com>
2014-11-07 22:31:17 +0800

    code

--- Codeforces/Codeforces Round #275/problemB/main.cpp
+++ Codeforces/Codeforces Round #275/problemB/main.cpp
@@ -1,1 +1,24 @@
+//二分也要优雅的啊
+//mid=l+(r-l)/2
+//mid=(r+l)>>1
+#include <iostream>
+#include <cstdio>
+using namespace std;
 
+int main(){
+    int cnt1,cnt2,x,y;
+    while(scanf("%d%d%d%d",&cnt1,&cnt2,&x,&y)==4){
+        int l=1;
+        int r=2e9;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if((mid-mid/x)>=cnt1&&(mid-mid/y)>=cnt2&&(mid-mid/(x+y))>=(cnt1+cnt2)){
+                r=mid;
+            }else{
+                l=mid+1;
+            }
+        }
+        printf("%d\n",r);
+    }
+    return 0;
+}
